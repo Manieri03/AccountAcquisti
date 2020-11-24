80eb2c8eb466087d7fbc22693a5c6a5c5678d30a
Alessio Manieri <mani.alexander03@gmail.com>
2020-11-24 12:58:22 +0100

    Aggiunti gli eventi click accedi, pulisci, stampa

--- LoginAcquisti/MainWindow.xaml.cs
+++ LoginAcquisti/MainWindow.xaml.cs
@@ -23,6 +23,102 @@ namespace LoginAcquisti
         public MainWindow()
         {
             InitializeComponent();
+
+            txtnome.Focus();
+            txtPrezzo.IsEnabled = false;
+            txtQuant.IsEnabled = false;
+            cmbProd.IsEnabled = false;
+            btnPulisci.IsEnabled = false;
+            btnStampa.IsEnabled = false;
+
+        }
+        private const string PASSW = "5Luglio";
+        private string[] prodotti = new string[] { "Felpa", "Maglietta", "Jeans", "Mutande", "Calzini"};
+
+        private void BtnAccedi_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtnome.Text != "" && txtpass.Text != "")
+            {
+                if (txtpass.Text == PASSW)
+                {
+                    txtnome.IsEnabled = false;
+                    txtpass.IsEnabled = false;
+                    btnAccedi.IsEnabled = false;
+
+                    txtPrezzo.IsEnabled = true;
+                    txtQuant.IsEnabled = true;
+                    cmbProd.IsEnabled = true;
+                    btnPulisci.IsEnabled = true;
+                    btnStampa.IsEnabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Password errata", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Error);
+                    txtpass.Text = "";
+                    txtpass.Focus();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Devi inserire tutte le credenziali per accedere", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Information);
+                txtnome.Text = "";
+                txtpass.Text = "";
+                txtnome.Focus();
+            }
+
+        }
+
+        private void CmbProd_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (var prod in prodotti)
+            {
+                cmbProd.Items.Add(prod);
+            }
+        }
+
+        private void BtnPulisci_Click(object sender, RoutedEventArgs e)
+        {
+            txtQuant.Text = "";
+            txtPrezzo.Text = "";
+            cmbProd.SelectedItem = -1;
+        }
+
+        private void BtnStampa_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtQuant.Text!="" && txtPrezzo.Text!= "" && cmbProd.SelectedIndex != -1)
+            {
+                try
+                {
+                    double quantita = Convert.ToDouble(txtQuant.Text);
+                    double prezzo = Convert.ToDouble(txtPrezzo.Text);
+
+                    double tot = quantita * prezzo;
+                    string stampa = $"il cliente {txtnome.Text} ha acquistato il prodotto {cmbProd.SelectedItem}, spendendo {txtPrezzo.Text} euro";
+                    lblAcquisti.Items.Add(stampa);
+
+                    txtQuant.Text = "";
+                    txtPrezzo.Text = "";
+                    cmbProd.SelectedIndex = -1;
+                    lblAcquisti.IsEnabled = true;
+                    btnRimuovi.IsEnabled = true;
+
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Ci sono degli errori nell'immissione dei dati", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Non sono stati immessi tutti i dati", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void BtnRimuovi_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
+
 }
